075e7b147206ec4139186153926b8281daaf3fbd
Link Doyle <linkdoyle@163.com>
2020-08-07 09:01:28 +0800

    [WIP] Add `DrawBoard` demo

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 import "./index.css";
-
+import { DrawBoard } from "./drawing/DrawBoard";
 console.log("Hello");
+
+const drawBoardContainer = document.getElementById("draw-board");
+if (drawBoardContainer) {
+  const drawBoard = new DrawBoard(drawBoardContainer);
+}
