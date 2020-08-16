9413371a53197dda8c007e1bf386b420f14e43f3
Link <linkdoyle@163.com>
2020-08-16 21:48:44 +0800

    [WIP] Exacting `Lines` componment from `ContentView`.

--- src/editor/view/ContentView.tsx
+++ src/editor/view/ContentView.tsx
@@ -1,30 +1,9 @@
-import React, {
-  useState,
-  useRef,
-  useEffect,
-  ReactElement,
-  useDebugValue,
-} from "react";
+import React, { useState, useRef, useEffect, ReactElement } from "react";
 import _ from "lodash";
 import { Caret } from "../editorReducer";
-import { useInterval } from "../hooks";
 import DebugView from "./DebugView";
-import Carets, { CaretMetric } from "./Carets";
-
-const parseLine = (line: string): ReactElement[] => {
-  const matches = line.match(/(\S+|\s+)/g);
-  if (matches) {
-    return matches.map((s, index) => (
-      <span key={index}>{s.replace(/ /g, "\u00A0")}</span>
-    ));
-  } else {
-    return [<span key={0}></span>];
-  }
-};
-
-const LineView = (props: { line: string }) => {
-  return <div className="editor-line">{parseLine(props.line)}</div>;
-};
+import Carets from "./Carets";
+import Lines, { useCaretMetrics } from "./Lines";
 
 const ContentContainer = (props: {
   lines: string[];
@@ -33,200 +12,33 @@ const ContentContainer = (props: {
   onTextDelete?: (line: number, startOffset: number, endOffset: number) => void;
   onCaretsChange?: (carets: Caret[]) => void;
 }): ReactElement => {
-  const [caretMetrics, setCaretMetrics] = useState<CaretMetric[]>([]);
-
-  const contentRef = useRef<HTMLDivElement>(null);
+  const { lines, carets, onTextInsert, onCaretsChange } = props;
+  const linesViewRef = useRef<HTMLDivElement>(null);
+  const measurerRef = useRef<HTMLDivElement>(null);
+  const [caretMetrics] = useCaretMetrics(linesViewRef, measurerRef, carets);
   const inputTextAreaRef = useRef<HTMLTextAreaElement>(null);
-  const editorTextMeasurerRef = useRef<HTMLDivElement>(null);
-
-  const [cursorVisibility, setCursorVisibility] = useState<
-    "visible" | "hidden"
-  >("visible");
-
-  useInterval(() => {
-    setCursorVisibility(cursorVisibility === "visible" ? "hidden" : "visible");
-  }, 500);
-
-  const measureElementText = (element: HTMLElement): number[] => {
-    if (!element.textContent) {
-      return [0];
-    }
-    const editorTextMeasurer = editorTextMeasurerRef.current!;
-    editorTextMeasurer.childNodes.forEach((e) => e.remove());
-    const clonedNode = element.cloneNode() as HTMLElement;
-    editorTextMeasurer.append(clonedNode);
-
-    const metricOffsets: number[] = [0];
-    for (const c of element.textContent) {
-      clonedNode.textContent += c;
-      metricOffsets.push(clonedNode.getBoundingClientRect().width);
-    }
-    return metricOffsets;
-  };
-
-  const handleLinesMouseUp = (
-    e: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    updateCursorPosition(e);
-  };
-
-  const handleLinesMouseMove = (
-    e: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    if (e.buttons & 1) {
-      updateCursorPosition(e);
-    }
-  };
-
-  const handleLinesMouseDown = (
-    e: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    // console.debug("handleLinesMouseDown");
-    updateCursorPosition(e);
-  };
-
-  const handleLinesMouseLeave = (
-    e: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    if (e.buttons & 1) {
-      updateCursorPosition(e);
-    }
-  };
-
-  const handleLinesBlur = (e: React.FocusEvent<HTMLDivElement>) => {
-    // updateCursorPosition(e.currentTarget);
-  };
-
-  const updateCursorPosition = (
-    event: React.MouseEvent<HTMLSpanElement | HTMLDivElement>
-  ) => {
-    // updateCursorPositionWithSelection(element);
-    updateCaretsPositionWithMeasurer(event);
-  };
-
-  const updateCaretsPositionWithMeasurer = (
-    event: React.MouseEvent<HTMLSpanElement | HTMLDivElement>
-  ) => {
-    const target = event.target as HTMLElement;
-    let span: HTMLSpanElement | null = null;
-    let metricIndex = -1;
-
-    if (target instanceof HTMLDivElement) {
-      let container = target;
-      if (target.classList.contains("editor-lines")) {
-        container = target.lastElementChild as HTMLDivElement;
-      }
-
-      if (container.classList.contains("editor-line")) {
-        const element = container.lastElementChild as HTMLElement;
-        if (element && element instanceof HTMLSpanElement) {
-          span = element;
-          metricIndex = span.textContent?.length ?? 0;
-        }
-      }
-    } else if (target instanceof HTMLSpanElement) {
-      span = target;
-    }
-
-    if (span) {
-      const metricOffsets = measureElementText(span);
-      if (metricIndex === -1) {
-        const domRect = target.getBoundingClientRect();
-        const offsetX = Math.max(event.clientX - domRect.x, 0);
-        metricIndex = 0;
-        for (let i = 0; i < metricOffsets.length; ++i) {
-          if (metricOffsets[i] >= offsetX) {
-            break;
-          }
-          metricIndex = i;
-        }
-      }
-      const lineElement = span.parentElement;
-      const spanIndex = _.indexOf(lineElement?.children, span);
-      const linesElement = lineElement?.parentElement;
-      const lineIndex = _.indexOf(linesElement?.children, lineElement);
-      const charOffset =
-        _(lineElement?.children)
-          .slice(0, spanIndex)
-          .sumBy((e) => e.textContent?.length ?? 0) + metricIndex;
-      props.onCaretsChange?.([
-        {
-          line: lineIndex,
-          offset: charOffset,
-        },
-      ]);
-    }
-  };
-
-  // update caretMetrics
-  useEffect(() => {
-    const linesView = contentRef.current;
-    if (!linesView) {
-      return;
-    }
-    const newCaretMetrics: CaretMetric[] = [];
-    for (const caret of props.carets) {
-      const lineView = linesView.children[caret.line];
-      let restOffset = caret.offset;
-      let spanElement: HTMLSpanElement | null = null;
-      for (const element of lineView.children) {
-        if (!(element instanceof HTMLSpanElement)) {
-          continue;
-        }
-        const currentLength = element.textContent?.length ?? 0;
-        if (restOffset > currentLength) {
-          restOffset -= currentLength;
-        } else {
-          spanElement = element;
-          break;
-        }
-      }
-      if (spanElement) {
-        newCaretMetrics.push({
-          focusElement: spanElement,
-          metricOffsets: measureElementText(spanElement),
-          metricIndex: restOffset,
-        });
-      }
-    }
-    setCaretMetrics(newCaretMetrics);
-  }, [props.carets]);
 
   useEffect(() => {
     inputTextAreaRef.current?.focus();
   });
-
-  useDebugValue(props.carets);
-  useDebugValue(caretMetrics);
-
-  const handleContentClick = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    // console.log("handleContentClick:");
-  };
 
   const [isCompositionMode, setCompositionMode] = useState(false);
   const handleCompositionStart = (
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     _: React.CompositionEvent<HTMLTextAreaElement>
   ) => {
-    // console.debug("handleCompositionStart");
     setCompositionMode(true);
   };
 
   const handleCompositionUpdate = (
     event: React.CompositionEvent<HTMLTextAreaElement>
   ) => {
-    // console.debug("handleCompositionUpdate");
     // TODO: show characters
   };
 
   const handleCompositionEnd = (
     event: React.CompositionEvent<HTMLTextAreaElement>
   ) => {
-    // console.debug(`handleCompositionEnd`);
-    // console.debug(event.data);
-    // console.debug(event.currentTarget.value);
     event.currentTarget.value = "";
     setCompositionMode(false);
   };
@@ -241,12 +53,12 @@ const ContentContainer = (props: {
     event.currentTarget.value = "";
     console.debug(insertedText);
 
-    const lastCaret = _.last(props.carets);
+    const lastCaret = _.last(carets);
     console.log(lastCaret);
     if (lastCaret) {
-      console.log(props.carets);
-      props.onTextInsert?.(lastCaret.line, lastCaret.offset, insertedText);
-      console.log(props.carets);
+      console.log(carets);
+      onTextInsert?.(lastCaret.line, lastCaret.offset, insertedText);
+      console.log(carets);
     }
   };
 
@@ -257,9 +69,7 @@ const ContentContainer = (props: {
         top: 0,
       };
     }
-
     const lastCaretMetric = _.last(caretMetrics);
-
     return lastCaretMetric
       ? {
           left:
@@ -275,23 +85,14 @@ const ContentContainer = (props: {
 
   return (
     <div className="editor-content">
-      <div className="editor-text-measurer" ref={editorTextMeasurerRef} />
-      <div
-        className="editor-lines"
-        ref={contentRef}
-        onClick={handleContentClick}
-        onMouseDown={handleLinesMouseDown}
-        onMouseMove={handleLinesMouseMove}
-        onMouseUp={handleLinesMouseUp}
-        onMouseLeave={handleLinesMouseLeave}
-        onBlur={handleLinesBlur}
-      >
-        {props.lines.map((value, index) => {
-          return <LineView key={index} line={value} />;
-        })}
-      </div>
+      <Lines
+        lines={lines}
+        linesRef={linesViewRef}
+        measurerRef={measurerRef}
+        onCaretsChange={onCaretsChange}
+      />
       <DebugView>
-        {props.carets.map((c, i) => {
+        {carets.map((c, i) => {
           return (
             <span key={i}>
               line: {c.line}, offset: {c.offset}{" "}
@@ -312,7 +113,6 @@ const ContentContainer = (props: {
         onCompositionUpdate={handleCompositionUpdate}
         onCompositionEnd={handleCompositionEnd}
         onCompositionStart={handleCompositionStart}
-        onFocus={() => console.log("focus")}
         style={{ ...calcCaretInputAreaPosition() }}
       />
     </div>
---
+++ src/editor/view/Lines.tsx
@@ -0,0 +1,183 @@
+import React, {
+  ReactElement,
+  useRef,
+  RefObject,
+  Dispatch,
+  SetStateAction,
+  useState,
+  useEffect,
+} from "react";
+import _ from "lodash";
+import { Caret } from "../editorReducer";
+import { CaretMetric } from "./Carets";
+
+const measureElementText = (
+  measurerRef: RefObject<HTMLDivElement | undefined>,
+  span: HTMLSpanElement
+): number[] => {
+  const measurer = measurerRef.current;
+  if (!measurer) {
+    return [0];
+  }
+  if (!span.textContent) {
+    return [0];
+  }
+  measurer.childNodes.forEach((e) => e.remove());
+  const clonedNode = span.cloneNode() as HTMLSpanElement;
+  measurer.append(clonedNode);
+
+  const metricOffsets: number[] = [0];
+  for (const c of span.textContent) {
+    clonedNode.textContent += c;
+    metricOffsets.push(clonedNode.getBoundingClientRect().width);
+  }
+  return metricOffsets;
+};
+
+export const useCaretMetrics = (
+  linesViewRef: RefObject<HTMLDivElement>,
+  measurerRef: RefObject<HTMLDivElement>,
+  carets: Caret[]
+): [CaretMetric[], Dispatch<SetStateAction<CaretMetric[]>>] => {
+  const [caretMetrics, setCaretMetrics] = useState<CaretMetric[]>([]);
+  console.log(carets);
+  console.log(caretMetrics);
+
+  // update caretMetrics
+  useEffect(() => {
+    const linesView = linesViewRef.current;
+    const measurer = measurerRef.current;
+    if (!linesView || !measurer) {
+      return;
+    }
+    const newCaretMetrics: CaretMetric[] = [];
+    for (const caret of carets) {
+      const lineView = linesView.children[caret.line];
+      let restOffset = caret.offset;
+      let spanElement: HTMLSpanElement | null = null;
+      for (const element of lineView.children) {
+        if (!(element instanceof HTMLSpanElement)) {
+          continue;
+        }
+        const currentLength = element.textContent?.length ?? 0;
+        if (restOffset > currentLength) {
+          restOffset -= currentLength;
+        } else {
+          spanElement = element;
+          break;
+        }
+      }
+      if (spanElement) {
+        newCaretMetrics.push({
+          focusElement: spanElement,
+          metricOffsets: measureElementText(measurerRef, spanElement),
+          metricIndex: restOffset,
+        });
+      }
+    }
+    setCaretMetrics(newCaretMetrics);
+  }, [linesViewRef, measurerRef, carets]);
+  return [caretMetrics, setCaretMetrics];
+};
+
+const Line = (props: { line: string }): ReactElement => {
+  const { line } = props;
+  const matches = line.match(/(\S+|\s+)/g);
+  return (
+    <div className="editor-line">
+      {matches ? (
+        matches.map((s, index) => (
+          <span key={index}>{s.replace(/ /g, "\u00A0")}</span>
+        ))
+      ) : (
+        <span key={0}></span>
+      )}
+    </div>
+  );
+};
+
+function Lines(props: {
+  lines: string[];
+  linesRef: RefObject<HTMLDivElement>;
+  measurerRef: RefObject<HTMLDivElement>;
+  onCaretsChange?: (carets: Caret[]) => void;
+}): ReactElement {
+  const { lines, linesRef, measurerRef, onCaretsChange } = props;
+  const updateCaretsPositionWithMeasurer = (
+    event: React.MouseEvent<HTMLSpanElement | HTMLDivElement>
+  ) => {
+    const target = event.target as HTMLElement;
+    let span: HTMLSpanElement | null = null;
+    let metricIndex = -1;
+
+    if (target instanceof HTMLDivElement) {
+      let container = target;
+      if (target.classList.contains("editor-lines")) {
+        container = target.lastElementChild as HTMLDivElement;
+      }
+
+      if (container.classList.contains("editor-line")) {
+        const element = container.lastElementChild as HTMLElement;
+        if (element && element instanceof HTMLSpanElement) {
+          span = element;
+          metricIndex = span.textContent?.length ?? 0;
+        }
+      }
+    } else if (target instanceof HTMLSpanElement) {
+      span = target;
+    }
+
+    if (span) {
+      const metricOffsets = measureElementText(measurerRef, span);
+      if (metricIndex === -1) {
+        const domRect = target.getBoundingClientRect();
+        const offsetX = Math.max(event.clientX - domRect.x, 0);
+        metricIndex = 0;
+        for (let i = 0; i < metricOffsets.length; ++i) {
+          if (metricOffsets[i] >= offsetX) {
+            break;
+          }
+          metricIndex = i;
+        }
+      }
+      const lineElement = span.parentElement;
+      const spanIndex = _.indexOf(lineElement?.children, span);
+      const linesElement = lineElement?.parentElement;
+      const lineIndex = _.indexOf(linesElement?.children, lineElement);
+      const charOffset =
+        _(lineElement?.children)
+          .slice(0, spanIndex)
+          .sumBy((e) => e.textContent?.length ?? 0) + metricIndex;
+      onCaretsChange?.([
+        {
+          line: lineIndex,
+          offset: charOffset,
+        },
+      ]);
+    }
+  };
+
+  return (
+    <>
+      <div className="editor-text-measurer" ref={measurerRef} />
+      <div
+        className="editor-lines"
+        ref={linesRef}
+        onMouseDown={(e) => updateCaretsPositionWithMeasurer(e)}
+        onMouseMove={(e) =>
+          e.buttons & 1 && updateCaretsPositionWithMeasurer(e)
+        }
+        onMouseUp={(e) => updateCaretsPositionWithMeasurer(e)}
+        onMouseLeave={(e) =>
+          e.buttons & 1 && updateCaretsPositionWithMeasurer(e)
+        }
+      >
+        {lines.map((value, index) => (
+          <Line key={index} line={value} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Lines;
